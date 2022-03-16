754d09173c3ced0f7175287bb492a0b668e9f367
David <Davidmaxsands@gmail.com>
2022-03-16 09:46:46 -0600

    commit tree c27240c8db93706ef38760f5f4e9cc91b6aa4e3b

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :record_unprocessable
+ 
 
   def show
     post = Post.find(params[:id])
@@ -8,16 +10,20 @@ class PostsController < ApplicationController
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
+  # rescue ActiveRecord::RecordInvalid => invalid
+  #   render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
 
   def post_params
     params.permit(:category, :content, :title)
+  end
+
+  def record_unprocessable(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
